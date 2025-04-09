d13cfc8e72c14b054798ca4d9332bc2b2789c56a
maksbuk007 <bukatinmaksimilian6@gmail.com>
2025-04-09 18:33:13 +0300

    Изменил инфу Кости

--- lib/users.ts
+++ lib/users.ts
@@ -206,9 +206,9 @@ export type User = {
     },
     {
       id: "student23",
-      username: "semenchkenko",
+      username: "semenchenko",
       password: "password",
-      name: "Семенчкенко Костя",
+      name: "Семенченко Костя",
       role: "student",
       class: "11",
     },
